3022c9b3e8a8070cfefa8d0f5bfdc3c3bcdc09e4
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-10-30 11:06:29 +0000

    how to create an object

---
+++ Object.js
@@ -0,0 +1,10 @@
+// objects are absically the blue print of the entity
+// they help us to define the charecteristics of the entity 
+
+const person = 
+{
+  name:'xyz', 
+  phoneno:12345,  // name,phoneno, address are the keyvalue
+  address:"2345A/c"
+}
+console.log(person);
